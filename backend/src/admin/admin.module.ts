8040cc718d30ab5f0ffac4db57f75d69f34602df
tithyarita <neth.tithyarita12345@gmail.com>
2026-04-21 08:14:12 +0700

    backend

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Admin } from './admin.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Admin])],
+})
 export class AdminModule {}
