796448b8dfb4dd6e206fc9d15acbe21b5fe0350b
tithyarita <neth.tithyarita12345@gmail.com>
2026-05-13 08:28:33 +0700

    redploy

--- project/src/views/Provider/ProviderRevenue.vue
+++ project/src/views/Provider/ProviderRevenue.vue
@@ -43,7 +43,7 @@
 <script setup>
 import SideBar from '../../components/provider_com/sideBar.vue'
 import PageHeader from '../../components/provider_com/pageHeader.vue'
-import RevenueStats from '../../components/provider_com/revenueStats.vue'
+// import RevenueStats from '../../components/provider_com/revenueStats.vue'
 import ProductsSellAnalysis from '../../components/provider_com/ProductsSellAnalysis.vue'
 import RecentOrder from '../../components/provider_com/recentOrder.vue'
 import TopCustomers from '../../components/provider_com/TopCustomers.vue'
