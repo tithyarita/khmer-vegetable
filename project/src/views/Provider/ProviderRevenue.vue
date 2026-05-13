e73d252f6fd3b3fd77b4c54a83c77cb6aec0fe8d
tithyarita <neth.tithyarita12345@gmail.com>
2026-05-13 08:15:30 +0700

    redeploy

--- project/src/views/Provider/ProviderRevenue.vue
+++ project/src/views/Provider/ProviderRevenue.vue
@@ -41,7 +41,7 @@
 </template>
 
 <script setup>
-import SideBar from '../../components/provider_com/SideBar.vue'
+import SideBar from '../../components/provider_com/sideBar.vue'
 import PageHeader from '../../components/provider_com/PageHeader.vue'
 import RevenueStats from '../../components/provider_com/RevenueStats.vue'
 import ProductsSellAnalysis from '../../components/provider_com/ProductsSellAnalysis.vue'
