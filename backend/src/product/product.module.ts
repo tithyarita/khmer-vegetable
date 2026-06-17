52cfbfd58872465336bbd1a795376becded973f1
Nuth69 <pravevinuth888@gmail.com>
2026-06-17 11:58:53 +0700

    fix

--- backend/src/product/product.module.ts
+++ backend/src/product/product.module.ts
@@ -6,11 +6,14 @@ import { ProductController } from './product.controller';
 import { HttpModule } from '@nestjs/axios';
 import { AuthModule } from '../auth/auth.module';
 import { orderItems } from '../users/order-items.entity';
+import { Cart } from '../cart/cart.entity';
+import { Favorite } from '../favorite/favorite.entity';
+import { Review } from '../review/review.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Product, orderItems]),
-    HttpModule, // ✅ IMPORTANT
+    TypeOrmModule.forFeature([Product, orderItems, Cart, Favorite, Review]),
+    HttpModule,
     AuthModule,
   ],
   providers: [ProductService],
